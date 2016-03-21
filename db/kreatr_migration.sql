d12cb34241883870702bbd79b0deb79574436d3a
James Traver <jtraverjr@gmail.com>
2016-03-21 12:38:56 -0500

    lets go

---
+++ db/kreatr_migration.sql
@@ -0,0 +1,6 @@
+# 1. CREATE DATABASE
+# 2. CONNECT TO DATABASE
+# 3. CREATE TABLES - at least 3 tables with 2-3 columns each
+# 4. There must be an id SERIAL PRIMARY KEY
+# 5. When done - take these, run in psql
+# 6. Connect to each database and list all the tables using \dt
